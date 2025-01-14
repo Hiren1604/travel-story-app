0ad512da2438958b0df11d08ca774598e3bf3ae8
hiren1604 <waghelahiren16@gmail.com>
2025-01-14 14:56:22 +0530

    Added Features

--- frontend/travel-story-app/src/pages/Auth/Login.jsx
+++ frontend/travel-story-app/src/pages/Auth/Login.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PasswordInput from '../../components/input/PasswordInput'
+import { useNavigate } from "react-router-dom"
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+
+
+  const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    if(!validateEmail(email)) {
+      setError("Please enter a valid amail address.");
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter the password.");
+      return;
+    }
+    setError("");
+
+    //Api Call
+    
+  }
   return (
     <div className='overflow-hidden bg-cyan-50 h-screen relative'>
       <div className='container flex justify-center items-center h-screen px-20 mx-auto'>
@@ -9,14 +36,15 @@ const Login = () => {
           <p className='text-cyan-50 text-lg'>Save all your exciting stories and memories</p>
         </div>
         <div className='p-16 w-1/3 h-[70vh] bg-white rounded-r-lg relative shadow-lg shadow-cyan-200/20 flex flex-col items-start justify-center'>
-          <form onSubmit={() => { }} className='w-full'>
+          <form onSubmit={handleLogin} className='w-full'>
             <h4 className='text-4xl font-bold mb-7'>Login</h4>
-            <input type="email" placeholder='Enter your Email' className='input-email' />
-            
+            <input type="text" placeholder='Enter your Email' className='input-email' value={email} onChange={({target})=>{setEmail(target.value)}}/>
+            <PasswordInput value={password} onChange={({target})=>{setPassword(target.value)}}/>
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
             <div>
-              <button className='btn-primary' type='submit'>SUBMIT</button>
+              <button className='btn-primary' type='submit'>LOGIN</button>
               <p className='text-sm text-slate-500 text-center'>OR</p>
-              <button type='submit' className='btn-light' onClick={() => { Navigate("/signup") }}>CREATE ACCOUNT</button>
+              <button type='button' className='btn-light' onClick={() => navigate("/signup")}>CREATE ACCOUNT</button>
             </div>
           </form>
         </div>
